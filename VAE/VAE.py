8cfb1b1827f1b2f07be6b87ac3a1a72645a83998
Adam Shusterman <adamshus@gmail.com>
2024-12-29 20:05:05 +0200

    finish VAE

--- VAE/VAE.py
+++ VAE/VAE.py
@@ -49,17 +49,36 @@ class Model(nn.Module):
             mu = torch.zeros((sample_size,self.latent_dim)).to(self.device)
         if logvar == None:
             logvar = torch.zeros((sample_size,self.latent_dim)).to(self.device)
-        #TODO
+        
+        z = self.z_sample(mu, logvar)
+
+        generated_samples = self.decoder(self.upsample(z).view(z.shape[0], 64, 7, 7))
+
+        return generated_samples
 
 
     def z_sample(self, mu, logvar):
-        #TODO
-        pass
+        return mu + (0.5*logvar).exp()*torch.randn_like(mu)
 
     def loss(self,x,recon,mu,logvar):
-        #TODO
-        pass
+        recon_error = F.binary_cross_entropy(recon, x, reduction='sum')
+
+        #from the HW solution assuming q ~ N(0,I)
+        kl_divergence = -0.5 * torch.sum((logvar + 1 + - (logvar.exp() + mu**2)))
+
+        return (recon_error + kl_divergence) / x.size(0) #ELBO is calculated per datapoint
 
     def forward(self, x):
-        #TODO
-        pass
+        encoded_x = self.encoder(x)
+
+        mu = self.mu(encoded_x.view(x.shape[0], encoded_x.shape[1] * encoded_x.shape[2] * encoded_x.shape[3]))
+        logvar = self.logvar(encoded_x.view(x.shape[0], encoded_x.shape[1] * encoded_x.shape[2] * encoded_x.shape[3]))
+
+        z = self.z_sample(mu, logvar)
+
+        recon = self.decoder(self.upsample(z).view(z.shape[0], 64, 7, 7))
+
+        return self.loss(x, recon, mu, logvar)
+
+
+
--- VAE/train.py
+++ VAE/train.py
@@ -5,18 +5,48 @@ import argparse
 import torch, torchvision
 from torchvision import transforms
 import numpy as np
-from VAE import model
+from VAE import Model
+from tqdm import trange, tqdm
+import matplotlib.pyplot as plt
+from torch.utils.tensorboard import SummaryWriter
 
-def train(vae, trainloader, optimizer, epoch):
+def train(vae, trainloader, optimizer, epoch, writer):
     vae.train()  # set to training mode
-    #TODO
+    loss_list = []
+
+    for inputs,_ in trainloader:
+        loss = vae(inputs.to(vae.device))
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        loss_list.append(-loss.item())
+
+    if writer:
+        writer.add_scalar("Epoch train ELBO", sum(loss_list) / len(loss_list), epoch)
+        writer.close()
 
 
-def test(vae, testloader, filename, epoch):
+def test(vae, testloader, filename, epoch, writer):
+    loss_list = []
+
     vae.eval()  # set to inference mode
     with torch.no_grad():
-        pass
-        #TODO
+        samples = vae.sample(100).cpu()
+        # a,b = samples.min(), samples.max()
+        # samples = (samples-a)/(b-a+1e-10) 
+        torchvision.utils.save_image(torchvision.utils.make_grid(samples),
+                                     './VAE/samples/' + filename.split('.')[0] + 'epoch%d.png' % epoch)
+        for inputs,_ in testloader:
+        
+            loss = vae(inputs.to(vae.device))
+
+            loss_list.append(-loss.item())
+
+    if writer:
+        writer.add_scalar("Epoch test ELBO", sum(loss_list) / len(loss_list), epoch)
+        writer.close()
 
 
 def main(args):
@@ -41,7 +71,7 @@ def main(args):
         trainset = torchvision.datasets.FashionMNIST(root='~/torch/data/FashionMNIST',
             train=True, download=True, transform=transform)
         trainloader = torch.utils.data.DataLoader(trainset,
-            batch_size=batch_size, shuffle=True, num_workers=2)
+            batch_size=args.batch_size, shuffle=True, num_workers=2)
         testset = torchvision.datasets.FashionMNIST(root='./data/FashionMNIST',
             train=False, download=True, transform=transform)
         testloader = torch.utils.data.DataLoader(testset,
@@ -57,14 +87,18 @@ def main(args):
     optimizer = torch.optim.Adam(
         vae.parameters(), lr=args.lr)
 
-    #TODO
+    writer = SummaryWriter(log_dir='logs/' + filename + '_logs')
+
+    for epoch in tqdm(range(args.epochs)):
+        train(vae, trainloader, optimizer, epoch, writer)
+        test(vae, testloader, filename, epoch, writer)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('')
     parser.add_argument('--dataset',
                         help='dataset to be modeled.',
                         type=str,
-                        default='mnist')
+                        default='fashion-mnist')
     parser.add_argument('--batch_size',
                         help='number of images in a mini-batch.',
                         type=int,
